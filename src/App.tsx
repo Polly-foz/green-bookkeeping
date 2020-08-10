baa87712cab26955a09ed540b03f48feb5e94737
polly-foz <polly.foz@gmail.com>
2020-08-10 13:30:37 +0800

    安装并测试styled-components和types/styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background: red;
+  position: relative;
+  & div{
+    width: 50px;
+    height: 50px;
+    background: blue;
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+    border: 1px solid yellowgreen;
+  }
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box><div></div></Box>
     </div>
   );
 }
