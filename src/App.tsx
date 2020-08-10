fa4c4a515c06b41db0df12a357df0e7e4ef11995
polly-foz <polly.foz@gmail.com>
2020-08-10 13:32:28 +0800

    删除无用代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
 
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background: red;
-  position: relative;
-  & div{
-    width: 50px;
-    height: 50px;
-    background: blue;
-    position: absolute;
-    top:50%;
-    left:50%;
-    transform: translate(-50%,-50%);
-    border: 1px solid yellowgreen;
-  }
-`
 
 function App() {
   return (
     <div className="App">
-      <Box><div></div></Box>
+        App
     </div>
   );
 }
