4bf35318db6248ffde7bb11fe8d6ab9ded38fb0c
polly-foz <polly.foz@gmail.com>
2020-08-12 09:03:28 +0800

    引入React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from 'react-router-dom';
 
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/details">明细</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">统计</Link>
+                        </li>
+                        <li>
+                            <Link to="/settings">设置</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-function App() {
-  return (
-    <div className="App">
-        App
-    </div>
-  );
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/details">
+                        <Details/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Route path="/settings">
+                        <Settings/>
+                    </Route>
+                    <Redirect exact from="/" to="/details"/>
+                    <Route path="*">
+                        <NoMatch />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function Settings() {
+    return <h2>Settings</h2>;
+}
+
+function Details() {
+    return <h2>Details</h2>;
+}
+
+function Statistics() {
+    return <h2>Statistics</h2>;
+}
+
+function NoMatch(){
+    return <h2>404</h2>
+}
